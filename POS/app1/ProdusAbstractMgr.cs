91c0f6f00a43a77a23a519191c6b8f4af6a7241a
PeterBrw <peter.giurgiu@gmail.com>
2021-04-03 22:14:24 +0300

    lab-4b

--- POS/app1/ProdusAbstractMgr.cs
+++ POS/app1/ProdusAbstractMgr.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using entitati;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace entitati
+namespace app1
 {
     public abstract class ProdusAbstractMgr
     {
