806a8672223c64cb85e98e605dccb09cf9c9ceff
PeterBrw <peter.giurgiu@gmail.com>
2021-04-29 11:28:21 +0300

    lab-8

---
+++ lab-8/WFApp/WFApp/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WFApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butMyFom = new System.Windows.Forms.Button();
+            this.but_schimba = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butMyFom
+            // 
+            this.butMyFom.Location = new System.Drawing.Point(327, 298);
+            this.butMyFom.Name = "butMyFom";
+            this.butMyFom.Size = new System.Drawing.Size(193, 65);
+            this.butMyFom.TabIndex = 0;
+            this.butMyFom.Text = "Catre MyForm";
+            this.butMyFom.UseVisualStyleBackColor = true;
+            this.butMyFom.Click += new System.EventHandler(this.butMyFom_Click);
+            // 
+            // but_schimba
+            // 
+            this.but_schimba.Location = new System.Drawing.Point(342, 476);
+            this.but_schimba.Name = "but_schimba";
+            this.but_schimba.Size = new System.Drawing.Size(178, 81);
+            this.but_schimba.TabIndex = 1;
+            this.but_schimba.Text = "Schimba";
+            this.but_schimba.UseVisualStyleBackColor = true;
+            this.but_schimba.Click += new System.EventHandler(this.but_schimba_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 698);
+            this.Controls.Add(this.but_schimba);
+            this.Controls.Add(this.butMyFom);
+            this.Name = "Form1";
+            this.Text = "Fereastra principala";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butMyFom;
+        private System.Windows.Forms.Button but_schimba;
+    }
+}
+
---
+++ lab-8/WFApp/WFApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butMyFom_Click(object sender, EventArgs e)
+        {
+            MyForm aform = new MyForm();
+            aform.Load += new EventHandler(MyForm_Load2);
+            aform.Show();
+        }
+
+        private void MyForm_Load2(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se va afisa fereastra MyFOrm!!!");
+        }
+
+        private void but_schimba_Click(object sender, EventArgs e)
+        {
+            this.Text = "Fereastra modificata la run-time!";
+        }
+    }
+}
